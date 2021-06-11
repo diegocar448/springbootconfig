4d6ec5022113b2029d185173ab6466f2e49c8370
Diego Cardoso <diegocar448@hotmail.com>
2021-06-11 10:18:00 -0300

    Dserver flag

--- src/main/java/com/digitalinnovationone/springbootconfig/AppController.java
+++ src/main/java/com/digitalinnovationone/springbootconfig/AppController.java
@@ -1,4 +1,18 @@
 package com.digitalinnovationone.springbootconfig;
 
-public class ApppController {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @Value("${app.message}")
+    private String appMessage;
+
+    //fazer a exibição no browser a partir do contexto de exibição
+    @GetMapping("/")
+    public String getAppMessage(){
+        return appMessage;
+    }
 }
